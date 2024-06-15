d84c58081deb7cffdf464a08ea113eee54dcea30
Sumeet <chhinasumeet@gmail.com>
2024-06-14 21:46:40 -0700

    Updated grads file

--- grads.py
+++ grads.py
@@ -3,6 +3,7 @@ from datasets import load_from_disk
 import geneformer
 from geneformer import TranscriptomeTokenizer
 from geneformer import perturber_utils as pu
+from geneformer import emb_extractor as ee
 import numpy as np
 from torch import nn
 import pickle
@@ -46,8 +47,8 @@ def expand(arr):
 
 model_type = "CellClassifier"
 num_classes = 2
-model_directory = "/u/scratch/s/schhina/geneformer_output/240509125208/240509_geneformer_cellClassifier_cm_classifier_test/ksplit1"
-n_samples = 4
+model_directory = "/u/scratch/s/schhina/temp_geneformer_output/240509125208/240509_geneformer_cellClassifier_cm_classifier_test/ksplit1"
+n_samples = 100
 
 model = pu.load_model(model_type, num_classes, model_directory, mode="eval")
 # model.train()
@@ -56,6 +57,7 @@ input_id_tensor = torch.load("/u/scratch/s/schhina/temp_files/input_ids.pt")
 atten_mask_tensor = torch.load("/u/scratch/s/schhina/temp_files/atten_mask.pt")
 labels_tensor = torch.load("/u/scratch/s/schhina/temp_files/labels.pt")
 lens_tensor = torch.load("/u/scratch/s/schhina/temp_files/lens.pt")
+downsampled_data_tensor = torch.load("/u/scratch/s/schhina/temp_files/downsampled_data.pt")
 
 output = model(input_ids=input_id_tensor[:n_samples].to("cuda"), attention_mask=atten_mask_tensor[:n_samples],)
 output.hidden_states[-1].retain_grad()
@@ -70,20 +72,8 @@ with open(token_dictionary_file, "rb") as f:
     gene_token_dict = pickle.load(f)
 
 token_gene_dict = {v: k for k, v in gene_token_dict.items()}
-print(token_gene_dict)
+# print(token_gene_dict)
 pad_token_id = gene_token_dict.get("<pad>")
-
-# embs_stack = pu.pad_tensor_list(
-#         [output.hidden_states[-1]],
-#         2048, # overall_max_len
-#         pad_token_id,
-#         model_input_size, # pu.get_model_input_size(model)
-#         1, # dim
-#         pu.pad_3d_tensor, 
-#     )
-
-print(embs_stack)
-print(embs_stack.shape)
 
 y = one_hot(labels_tensor)
 loss_fn = nn.CrossEntropyLoss()
@@ -97,13 +87,37 @@ loss.backward()
 # mean_embs.backward()
 # print(mean_embs.grad)
 
+embs_stack = pu.pad_tensor_list(
+        [output.hidden_states[-1].grad],
+        2048, # overall_max_len
+        pad_token_id,
+        model_input_size, # pu.get_model_input_size(model)
+        1, # dim
+        pu.pad_3d_tensor, 
+    )
+
+# print(embs_stack)
+# print(embs_stack.shape)
+
+embd_df = ee.label_gene_embs(embs_stack, downsampled_data_tensor, token_gene_dict)
+
 hidden_grad = output.hidden_states[-1].grad
 print(hidden_grad[0])
 print(hidden_grad.shape)
 
+print(embd_df.head())
+print(embd_df.shape)
+
+l2_norms = np.linalg.norm(embd_df, axis=1, ord=1)
+gene_norms = dict(zip(embd_df.index, l2_norms))
+print(gene_norms)
+for k, v in gene_norms.items():
+    if v != 0:
+        print((k, v))
+
 norms = torch.linalg.norm(hidden_grad, dim=2, ord=2)
-print(norms)
-print(norms.shape)
+# print(norms)
+# print(norms.shape)
 
 # print(output.logits.grad)
 # print(input_id_tensor.grad)
